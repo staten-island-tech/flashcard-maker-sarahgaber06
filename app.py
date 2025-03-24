334d2d5bfdeb29b553a41717feade42c30fae363
sarahgaber06 <sarahg6536@gmail.com>
2025-03-24 09:24:28 -0400

    flashcards

---
+++ app.py
@@ -0,0 +1,15 @@
+#You will be creating an application that has two modes
+
+#Teacher Mode
+
+#In teacher mode you will be asking the user to input a word/phrase and the
+#answer as a key:value pair. These pairs will be written to a json file called
+#FlashCards.json
+
+#Student Mode
+#In student mode you will present the student with the
+#phrases/words/images and the user will type the answer in the terminal.
+#Keep a tally of correct answers and provide a score. Give students bonus
+#points for “Streaks”
+
+class 
