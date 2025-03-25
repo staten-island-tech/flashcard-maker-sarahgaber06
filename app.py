b9a10a70d9f905b7c55eb52d4a921dfbdd1cea55
sarahgaber06 <sarahg6536@gmail.com>
2025-03-25 09:24:28 -0400

    flashcards

--- app.py
+++ app.py
@@ -6,10 +6,11 @@
 #answer as a key:value pair. These pairs will be written to a json file called
 #FlashCards.json
 
+
+
 #Student Mode
 #In student mode you will present the student with the
 #phrases/words/images and the user will type the answer in the terminal.
 #Keep a tally of correct answers and provide a score. Give students bonus
 #points for “Streaks”
 
-class 
