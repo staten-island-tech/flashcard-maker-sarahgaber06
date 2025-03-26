27dd84e78409598dcb66c4922c78eb3a430808b4
sarahgaber06 <sarahg6536@gmail.com>
2025-03-26 09:25:11 -0400

    flashcards

--- app.py
+++ app.py
@@ -5,9 +5,15 @@
 #In teacher mode you will be asking the user to input a word/phrase and the
 #answer as a key:value pair. These pairs will be written to a json file called
 #FlashCards.json
-
-
-
+class History:
+    def __init__(self, emperor, year, empire, date, image=None):
+        self.emperor = emperor
+        self.year = year
+        self.empire = empire
+        self.date = date
+    def display_info(self):
+        
+        
 #Student Mode
 #In student mode you will present the student with the
 #phrases/words/images and the user will type the answer in the terminal.
